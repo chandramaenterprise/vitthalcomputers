d2a21448ea30f1ecc3af96f6a046c12ab687da05
mahesh <106306297+chandramaenterprise@users.noreply.github.com>
2023-02-17 14:55:28 +0530

    Add files via upload

---
+++ laptops.hlsl
@@ -0,0 +1,81 @@
+1
+HP ElliteBook x360 1030 G3 Touch Screen
+Touch Screen Gorilla Glass Protection <br>Full Hd IPS +PSR Anti-Glare Display<br>Metal Body Natural Silver Color<br>Intal core i5 8 gen<16 GB DDR 4 RAM<br>256 GB NVME SSD<br>Intel UHD 620 Graphics<br>Fingerprint Sensor<br>Backlite keyword <br>4 g Sim Crd Support <br>Light And Slim
+
+2
+HP Pavilion x360 Convertible
+HP Pavilion x360 Convertible<br>x360 Touch Screen display<br>Intal Core i5 10 gen <br>16 GB RAm <br>256 GB SSD<br>4 GB Intal iris Graphis Card<br>Fingerprint Sensor<br>Backlite keyword<br><Fast Charging<br>
+
+
+4
+ HP 14s-dy2507TU
+HP 14s<br> Intel Core i3-11 gen<br> 8GB RAM/256GB SSD<br> 14 inch<br> Micro-Edge<br> Anti-Glare display<br> FHD Laptop<br>Alexa Built-in/Win 11<br>Intel UHD Graphics/Dual Speakers/
+
+5
+HP Pavilion 14-dv2014TU
+ HP Pavilion<br> 10th Gen Intel Core i5<br> 16GB RAM/512GB SSD <br>14 inch(35.6cm) IPS Micro-Edge FHD display<br>Intel Iris Xe Graphics<br>Windos 11<br>Alexa Built-in<br>Backlit keyword<br> Natural Silver Color
+
+ 6
+HP lightBook 840 G4 Touch Screen
+Hp LightBook 840 G4 Touch Screen<br>Intal Core i5 7 gen<br>Touch Screen HD display<br>8 GB DDR4 RAM<br>Backlit keyword<br>Fingerprint Screen<br>Type C Port<br>
+
+7
+HP ElliteBook 820 G3 12.5 inch
+HP ElliteBook 820 G3 12.5 inch<br>Intal Core i5 6 gen<br>8 GB DDR4 RAM<br>256 GB SSD<br>Backlit keyword<br> Type C Port
+
+HP ElliteBook 840 G3 Touch Screen 14 inch
+HP ElliteBook 840  G3 Touch Screen <br>Intal Core i5 6 gen<br>8 GB DDR4 RAM<br>256 GB SSD<br>Intal 4GB Graphics Card<br>New Addapter New Battry<br>
+
+Dell Latitude e 5480
+Dell Latitude e 5480<br>Intel Core i7 7 gen<br>8 GB RAM <br>512 GB SSD<br>4 GB Intel UHD 650 Graphics Card<br>14 inch FHD display<br><Backlit keyword<br><New Addapter New Battry<br>
+
+Dell Latitude e7470 Touch Screen
+
+
+
+
+
+<div class ="brand"><h1 id="mi">______________________<br>______________________<br>_____MI_____</h1></div>
+
+
+<div class="laptop">
+		
+		   <div class="box">
+			<button onclick="document.getElementById('myImage').src='img2.jpg'"></button>
+			<img src="m1.jpg" alt="Girl in a jacket" width=480px height=480px>
+	<div class ="text-small"><p id="m1"><b>Mi Notebook 14 </b></p></div>
+	<div class ="btni"><button type="button" onclick='document.getElementById("m1").innerHTML = "Intal Core i5 10 gen<br>8GB RAM 512GB SSD <br>Thin And Light<br>14 inch FHD Display<br>UHD 620 Graphics<br><br>"'>Click For More Informaction</button><br><br>
+	</div>
+	
+		   </div>
+		   
+		   <div class="box">
+			<button onclick="document.getElementById('myImage').src='img2.jpg'"></button>
+			<img src="m2.jpg" alt="Girl in a jacket" width=480px height=480px>
+	<div class ="text-small"><p id="m2"><b>MI Notebook Horizon Edition 14 </b></p></div>
+	<div class="btni"><button type="button" onclick='document.getElementById("m2").innerHTML = "MI Notebook Horizon Edition 14 <br>Intel Core i7-10510U 10th Gen<br> Thin and Light Laptop <br>8GB RAM 512GB SSD <br> Nvidia MX350 2GB Graphics<br>Grey 1.35Kg <br>"'>Click For More Informaction</button><br><br>
+	</div>
+		   </div>
+		   <div class="box">
+	<button onclick="document.getElementById('myImage').src='img2.jpg'"></button>
+	<img src="m3.jpg" alt="Girl in a jacket" width=480px height=480px>
+	<div class ="text-small"><p id="m3"><b>Mi Notebook Pro</b></p></div>
+	<div class="btni"><button type="button" onclick='document.getElementById("m3").innerHTML = "Mi Notebook Pro<br>14 inch QHD+ IPS Anti-Glare Display <br>Intel Core I5-11300H 11th Gen <br>Thin & Light Laptop <br>16GB RAM 512GB SSD <br>Iris Xe Graphics <br>Backlit Keyboard/FP Sensor/1.4 Kg<br>"'>Click For More Informaction</button><br><br>
+	</div>
+		   </div>
+		   <div class="box">
+			<button onclick="document.getElementById('myImage').src='img2.jpg'"></button>
+			<img src="m4.jpg" alt="Girl in a jacket" width=480px height=480px>
+			<div class ="text-small"><p id="m4"><b>Xiaomi Mi Notebook Pro Max</b></p></div>
+			<div class="btni"><button type="button" onclick='document.getElementById("m4").innerHTML = "Xiaomi Mi Notebook Pro Max <br>11th Gen Intel Core i5-11320H <br>Thin & Light <br>16GB RAM 512GB SSD <br>Iris Xe Graphics<br>14 inch QHD+ Display 2.5K Resolution<br>Thunderbolt 4<br> Fingerprint Sensor/1.4 Kg<br>"'>Click For More Informaction</button><br><br>
+			</div>
+				   </div>
+				   <div class="box">
+					<button onclick="document.getElementById('myImage').src='img2.jpg'"></button>
+					<img src="m5.jpg" alt="Girl in a jacket" width=480px height=480px>
+					<div class ="text-small"><p id="m5"><b>Xiaomi Notebook Pro 120G </b></p></div>
+					<div class="btni"><button type="button" onclick='document.getElementById("m5").innerHTML = "Xiaomi Notebook Pro 120G <br>12th Gen Intel i5-12450H<br> Thin & Light<br>16GB LPDDR5 RAM 512GB SSD<br>Nvidia MX550 Graphic<br>14 inch  2.5K Resolution/120Hz Display<br>Thunderbolt 4/FP Sensor/1.5Kg<br>"'>Click For More Informaction</button><br><br>
+					</div>
+						   </div>
+	</div>
+       
